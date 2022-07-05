7be4c7c7883eed145a8677808da681c59df86a0e
meior2543 <56358092+meior2543@users.noreply.github.com>
2022-07-05 10:02:35 +0700

    test

--- main.py
+++ main.py
@@ -81,11 +81,11 @@ global lr_cleanliness
 global lr_price
 global lr_food
 
-lr_service = pickle.load(open('LR\LR_service.pkl', 'rb'))
+# lr_service = pickle.load(open('LR\LR_service.pkl', 'rb'))
 # lr_atmosphere = pickle.load(open('LR\LR_atmosphere.pkl', 'rb'))
 # lr_cleanliness = pickle.load(open('LR\LR_cleanliness.pkl', 'rb'))
 # lr_price = pickle.load(open('LR\LR_price.pkl', 'rb'))
-lr_food = pickle.load(open('LR\LR_food.pkl', 'rb'))
+# lr_food = pickle.load(open('LR\LR_food.pkl', 'rb'))
 
 """Real Service"""
 @app.get("/classify_review")
@@ -96,18 +96,21 @@ def classify_review(text:str = 'ข้อความรีวิวร้าน
     text_count_vec = convert_to_cnt_vec(text_tk)
     # return text_count_vec
 
-    text_pred_s = lr_service.predict(text_count_vec)[0]
+    # text_pred_s = lr_service.predict(text_count_vec)[0]
     # text_pred_a = lr_atmosphere.predict(text_count_vec)[0]
     # text_pred_c = lr_cleanliness.predict(text_count_vec)[0]
     # text_pred_p = lr_price.predict(text_count_vec)[0]
-    text_pred_f = lr_food.predict(text_count_vec)[0]
+    # text_pred_f = lr_food.predict(text_count_vec)[0]
     
-    return {'service': str(text_pred_s), 
+    # return {
+            # 'service': str(text_pred_s), 
             # 'atmosphere': str(text_pred_a), 
             # 'cleanliness': str(text_pred_c), 
             # 'price': str(text_pred_p), 
-            'food(taste)': str(text_pred_f)}
-    return text
+            # 'food(taste)': str(text_pred_f)
+            # }
+    return text_count_vec
+    # return text
 
 if __name__ == '__main__':
    uvicorn.run(app, host="0.0.0.0", port=8069, debug=True) 
